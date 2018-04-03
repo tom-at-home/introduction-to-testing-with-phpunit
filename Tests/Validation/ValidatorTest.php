f5fdd88916ff15fe8f3e236b43d4b34608568753
Tomasz <tomasz.urbaniak@me.com>
2018-04-03 19:47:13 +0200

    Lektion 10

---
+++ Tests/Validation/ValidatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Acme\Tests;
+
+use Acme\Http\Response;
+use Acme\Http\Request;
+use Acme\Validation\Validator;
+
+class ValidatorTest extends \PHPUnit\Framework\TestCase {
+
+    public function testGetIsValidReturnsTrue()
+    {
+        $request = new Request([]);
+        $response = new Response($request);
+        $validator = new Validator($request, $response);
+
+        $validator->setIsValid(true);
+        $this->assertTrue($validator->getIsValid());
+    }
+
+    public function testGetIsValidReturnsFalse()
+    {
+        $request = new Request([]);
+        $response = new Response($request);
+        $validator = new Validator($request, $response);
+
+        $validator->setIsValid(false);
+        $this->assertFalse($validator->getIsValid());
+    }
+}
